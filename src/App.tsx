ceefebaf335e4153f9e5905e1051d04e9f84bd3e
erliwanderson <wandersonerli.dev@gmail.com>
2024-03-13 19:24:21 -0300

    :recycle: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import {router} from './routes'
+import {RouterProvider} from 'react-router-dom'
 function App() {
   return (
-    <>
-      <h1>Projeto Cripto</h1>
-    </>
+   <RouterProvider router={router} />
   );
 }
 
 export default App;
+
+
+//https://coinlib.io/api/v1/coinlist?key=418f0f2d7e92de8b&pref
